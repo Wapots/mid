09eb0698534e83b14ae1e40a4df3b6dac95d5d75
Wapots <114584917+Wapots@users.noreply.github.com>
2022-11-25 00:30:15 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php 
+// Joeshua Luis Paraguison
+//BSIT-4
+
+//php variable
+$firstname = "Joeshua";
+//php echo
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+// data types
+$year 2023;
+var_dump($year);
+
+//php string
+echo "<br>";
+$string = "Hello Baby";
+
+//php number
+$x=12345
+var_dump(is_int($x));
+
+//php math
+echo "<br>";
+echo 1+1;
+echo "<br>";
+echo 1-1;
+echo "<br>";
+echo 1*1;
+echo "<br>";
+echo 1/1;
+echo "<br>";
+echo 1%1 ;
+//php constant
+defind("GREETING","WELCOME TO ULS ");
+echo GREETING;
+
+//php operators
+//arithmetic operator
+echo "<br>";
+echo 1+1;
+echo "<br>";
+echo 1-1;
+echo "<br>";
+echo 1*1;
+echo "<br>";
+echo 1/1;
+echo "<br>";
+echo 1%1 ;
+
+// php if..Else..if
+if(Surname ! = "Paraguison pasado"){
+    echo "wrong username";
+}
+    
+else if($password != "password") {
+    echo "wrong password";
+}
+else {
+    echo "succesfull log in";
+}
+
+//php switch
+$var ="b";
+switch ($var){
+    case 'a':
+     echo "cake";
+     break
+     case 'b':
+     echo "Creampie";
+     break;
+     case 'c':
+     echo "lollipop";
+     break;
+     default:
+     echo "dessert not available"
+
+}
+//php loops
+for (i=0,1<=10, $i++){
+    echo $1
+}
+//php function
+function
+getWelcomeMessage($ad);{
+    echo $name;
+}
+message('Joeshua')
+
+echo "<br>";
+
+?>
+
+
